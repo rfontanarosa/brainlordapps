e06baaca826a30c794a503961f142708e8ad09bb
Roberto Fontanarosa <roberto.fontanarosa@pronetics.it>
2016-01-12 13:33:24 +0100

    BRANDISH 2

---
+++ apps/brandish2/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'brandish2');
+
+define('TITLE', 'Brandish 2: The Planet Buster - Translation Tool');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/db/brandish2.db');
+define('LAST_ENTRY', 955);
+define('NEWLINECHAR', '{01}');
+define('NEWLINE_REPLACE', 0);
