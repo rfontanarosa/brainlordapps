7d21d52c98b9a4067d0e430bedf5d13cfd58cd8e
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2019-05-21 17:21:21 +0200

    spike mcfange - initial commit

---
+++ apps/spike/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'spike');
+
+define('TITLE', 'The Twisted Tales of Spike McFang');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/db/spike.db');
+define('LAST_ENTRY', 343);
+define('NEWLINECHAR', '{f9}');
+define('NEWLINE_REPLACE', 0);
