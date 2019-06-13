38ce948e5938838d950339c9579215a3be2b4bb4
rfontanarosa <robertofontanarosa@gmail.com>
2019-06-13 10:30:26 +0200

    bof bugfix: wrong max id

--- apps/bof/config.inc.php
+++ apps/bof/config.inc.php
@@ -7,6 +7,6 @@ define('APPLICATION_ID', 'bof');
 define('TITLE', 'Breath of Fire');
 
 define('SQLITE_FILENAME', RESOURCE_PATH . '/db/bof.db');
-define('LAST_ENTRY', 1700);
+define('LAST_ENTRY', 1701);
 define('NEWLINECHAR', '{02}');
 define('NEWLINE_REPLACE', 0);
