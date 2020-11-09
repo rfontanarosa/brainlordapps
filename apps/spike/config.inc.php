f66ca5a6aafe549bce1c51ac61f1650b859c1078
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2020-11-09 13:38:34 +0100

    spike - initial commit

--- apps/spike/config.inc.php
+++ apps/spike/config.inc.php
@@ -3,10 +3,22 @@
 require_once '../../config.inc.php';
 
 define('APPLICATION_ID', 'spike');
+define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 
 define('TITLE', 'The Twisted Tales of Spike McFang');
 
 define('SQLITE_FILENAME', RESOURCE_PATH . '/spike/db/spike.db');
-define('LAST_ENTRY', 343);
-define('NEWLINECHAR', '{f9}');
+define('LAST_ENTRY', 295);
+define('NEWLINECHAR', '');
 define('NEWLINE_REPLACE', 0);
+
+function textClean($text) {
+  if (defined('NEWLINE_REPLACE') && NEWLINE_REPLACE && defined('NEWLINECHAR')) {
+    $text = str_replace(PHP_EOL, NEWLINECHAR, $text);
+  }
+  return $text;
+}
+
+function tableClean($text) {
+  return $text;
+}
