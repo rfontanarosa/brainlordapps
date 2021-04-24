363b4b19da1598d15b20c413d55e1e362e27b6d9
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-04-23 21:16:47 -0500

    lufia - limit increased by 10

--- apps/lufia/config.inc.php
+++ apps/lufia/config.inc.php
@@ -8,7 +8,7 @@ define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 define('TITLE', 'Lufia &amp; the Fortress of Doom');
 
 define('SQLITE_FILENAME', RESOURCE_PATH . '/lufia/db/lufia.sqlite3');
-define('LAST_ENTRY', 3051);
+define('LAST_ENTRY', 3061);
 define('NEWLINECHAR', '-----');
 define('NEWLINE_REPLACE', 0);
 
