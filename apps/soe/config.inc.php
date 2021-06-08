628dc979e0668e044359d7b697cd9bb477c983ee
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-06-07 22:56:57 -0500

    soe - new line management

--- apps/soe/config.inc.php
+++ apps/soe/config.inc.php
@@ -7,10 +7,10 @@ define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 
 define('TITLE', 'Secret of Evermore');
 
-define('SQLITE_FILENAME', RESOURCE_PATH . '/soe/db/soe.db');
+define('SQLITE_FILENAME', RESOURCE_PATH . '/soe/db/soe.sqlite3');
 define('LAST_ENTRY', 3002);
-define('NEWLINECHAR', '-----');
-define('NEWLINE_REPLACE', 0);
+define('NEWLINECHAR', PHP_EOL);
+define('NEWLINE_REPLACE', 1);
 
 function textClean($text) {
   if (defined('NEWLINE_REPLACE') && NEWLINE_REPLACE && defined('NEWLINECHAR')) {
