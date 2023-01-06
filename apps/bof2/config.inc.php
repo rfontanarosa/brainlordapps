73fd8ab6d32159e56540d57cf191231f69fc7999
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-01-06 15:10:59 +0100

    bof2 - skeleton app

---
+++ apps/bof2/config.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'bof2');
+define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
+
+define('TITLE', 'Breath of Fire 2');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/bof2/db/bof2.sqlite3');
+define('LAST_ENTRY', 428);
+define('NEWLINECHAR', '-----');
+define('NEWLINE_REPLACE', 0);
+
+function textClean($text) {
+  if (defined('NEWLINE_REPLACE') && NEWLINE_REPLACE && defined('NEWLINECHAR')) {
+    $text = str_replace(PHP_EOL, NEWLINECHAR, $text);
+  }
+  return $text;
+}
+
+function tableClean($text) {
+  return $text;
+}
