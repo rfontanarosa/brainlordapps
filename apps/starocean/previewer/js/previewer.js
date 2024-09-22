21ae26ac336025a45f392a2e7c1fac58215a6252
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-09-22 12:00:32 +0200

    general improvements

--- apps/starocean/previewer/js/previewer.js
+++ apps/starocean/previewer/js/previewer.js
@@ -2,112 +2,135 @@ String.prototype.replaceAt = function (index, replacement) {
   return (this.slice(0, index) + replacement + this.slice(index + replacement.length));
 };
 
-function staroceanTextClean(text) {
-  text = text.replace(/<RATIX>/g, "Ratix");
-  text = text.replace(/<MILLY>/g, "Milly");
-  text = text.replace(/<DORN>/g, "Dorn");
-  text = text.replace(/<RONIXIS>/g, "Ronixis");
-  text = text.replace(/<IRIA>/g, "Iria");
-  text = text.replace(/<CIUS>/g, "Cius");
-  text = text.replace(/<JOSHUA>/g, "Joshua");
-  text = text.replace(/<TINEK>/g, "Tinek");
-  text = text.replace(/<MARVEL>/g, "Marvel");
-  text = text.replace(/<ASHLAY>/g, "Ashlay");
-  text = text.replace(/<PERISIE>/g, "Perisie");
-  text = text.replace(/<FEAR>/g, "Fear");
-  text = text.replace(/<PAUSE><..>/g, "");
-  text = text.replace(/<COLOR><..>/g, "");
-  text = text.replace(/<WAIT><CLOSE>/g, "");
+function replaceText(regexes, text) {
+  return regexes.reduce((curr, [pattern, replacement]) => curr.replace(pattern, replacement), text)
+}
+
+const STAROCEAN_REGEXES = [
+  [/<RATIX>/g, "WWWWWWWW"],
+  [/<MILLY>/g, "Milly"],
+  [/<DORN>/g, "Dorn"],
+  [/<RONIXIS>/g, "Ronixis"],
+  [/<IRIA>/g, "Iria"],
+  [/<CIUS>/g, "Cius"],
+  [/<JOSHUA>/g, "Joshua"],
+  [/<TINEK>/g, "Tinek"],
+  [/<MARVEL>/g, "Marvel"],
+  [/<PERISIE>/g, "Ashlay"],
+  [/<FEAR>/g, "Fear"],
+  [/<PAUSE><..>/g, ""],
+  [/<COLOR><..>/g, ""],
+  [/<WAIT><CLOSE>/g, ""],
+  [/<CLOSE>/g, ""],
+  [/<CODE 88><..>/g, ""],
+];
+
+function staroceanGetBoxProperties(boxType) {
+  switch (boxType) {
+    default:
+      return {charLimit: 208, lineLimit: 4};
+  }
+}
+
+function staroceanReplaceText(text) {
+  text = text.replaceAll("<WAIT>\n", "\r");
+  text.match(/<PAD><..><..>/g)?.forEach(matchResult => {
+    console.log(matchResult);
+    const width = parseInt(matchResult.slice(6, 8), 16);
+    // const height = parseInt(matchResult.slice(10, 12), 16);
+    text = text.replaceAll(matchResult, "\t".repeat(width));
+  });
   return text;
 }
 
-function staroceanPreviewBox(
-  previewContainerSelector,
-  text,
-  boxIndex,
-  boxType
-) {
-  text = text.replaceAll("<WAIT>\n", "\r");
-  text = staroceanTextClean(text);
+function staroceanRenderPreview(containerSelector, text, textId, boxType) {
+  const {charLimit, lineLimit} = staroceanGetBoxProperties(boxType);
+  text = replaceText(STAROCEAN_REGEXES, text);
+  text = staroceanReplaceText(text);
   let newText = "";
-  const charLimit = 208;
-  const lineLimit = 4;
+  let spaceIndex = -1;
   let charCounter = 0;
   let lineCounter = 1;
-  let spacePosition = -1;
   for (let i = 0; i < text.length; i++) {
     const utf16char = text.charAt(i);
     newText += utf16char;
     // console.log(i, utf16char, charCounter, lineCounter, spacePosition);
-    if (utf16char === "\r") {
-      charCounter = 0;
-      lineCounter = 1;
-      spacePosition = -1;
-    } else if (utf16char === "\n") {
-      charCounter = 0;
-      spacePosition = -1;
-      if (lineCounter === lineLimit) {
-        newText = newText.slice(0, -1) + "\r";
+    switch (utf16char) {
+      case "\r":
+        spaceIndex = -1;
+        charCounter = 0;
         lineCounter = 1;
-      } else {
+        break;
+      case "\n":
+        spaceIndex = -1
+        charCounter = 0;
         lineCounter += 1;
-      }
-    } else {
-      if (utf16char === " ") {
-        spacePosition = i;
+        if (lineCounter > lineLimit) {
+          newText = newText.slice(0, -1) + "\r";
+        }
+        break;
+      case " ":
+        spaceIndex = i;
         charCounter += hashcharlist[utf16char] ? hashcharlist[utf16char] : 0;
-      } else {
+        break;
+      default:
         charCounter += hashcharlist[utf16char] ? hashcharlist[utf16char] : 0;
         if (charCounter > charLimit) {
-          if (spacePosition !== -1) {
+          if (spaceIndex !== -1) {
             if (lineCounter >= lineLimit) {
-              newText = newText.replaceAt(spacePosition, "\r");
+              newText = newText.replaceAt(spaceIndex, "\r");
               lineCounter = 1;
             } else {
-              newText = newText.replaceAt(spacePosition, "\n");
+              newText = newText.replaceAt(spaceIndex, "\n");
               lineCounter += 1;
             }
           }
-          spacePosition = -1;
+          spaceIndex = -1;
         }
-      }
     }
   }
 
   // console.log(newText.replaceAll('\r', '<BOX>').replaceAll('\n', '<LINE>'));
 
-  const previewContainer = $(`#${previewContainerSelector}`);
+  const previewContainer = document.getElementById(containerSelector);
   let dialogs = newText.split("\r");
   dialogs = dialogs.filter((element) => element !== "");
   dialogs.forEach((dialog, index) => {
     const previewBoxClass = "starocean-preview-box";
-    const previewBoxId = `dialog-${boxIndex}-${index}`;
-    const previewBox = `<div class="${previewBoxClass}" id="${previewBoxId}">\
-            <div class="bgimage">\
-                <div class="chars"></div>\
-            </div>\
-            <div class="infobox">\
-                <div class="counter1"></div>\
-                <div class="counter2"></div>\
-                <div class="counter3"></div>\
-                <div class="counter4"></div>\
-                <div class="alert"></div>\
-            </div>\
-        </div>`;
-    previewContainer.append(previewBox);
+    const previewBoxId = `dialog-${textId}-${index}`;
+    const previewBox = `
+      <div class="${previewBoxClass}" id="${previewBoxId}">\
+        <div class="bgimage">\
+          <div class="chars"></div>\
+        </div>\
+        <div class="infobox">\
+          <div class="counter1"></div>\
+          <div class="counter2"></div>\
+          <div class="counter3"></div>\
+          <div class="counter4"></div>\
+          <div class="alert"></div>\
+        </div>\
+      </div>
+    `;
+    previewContainer.innerHTML += previewBox;
 
     let indexLine = 0;
     let picturestring = "";
     let alert = "";
-    const counter = [0, 0, 0, 0];
+    const charCounters = new Array(lineLimit).fill(0);
+    const padCounters = new Array(lineLimit).fill(0);
 
     for (let i = 0; i < dialog.length; i++) {
       const utf16char = dialog.charAt(i);
       const utf16int = utf16char.charCodeAt();
       let buffer = "";
       if (hashcharlist[utf16char] > 0) {
-        counter[indexLine] += hashcharlist[utf16char];
+        charCounters[indexLine] += hashcharlist[utf16char];
         buffer = `<div class="starocean-font1 starocean-font1-${utf16int}"></div>`;
+      } else if (utf16char === "\t") {
+        charCounters[indexLine] += 1;
+        padCounters[indexLine] += 1;
+        buffer = '<div style="display: inline-block; width: 1px;"></div>';
       } else if (utf16char === "\n") {
         buffer = "<br />";
         indexLine++;
@@ -119,28 +142,26 @@ function staroceanPreviewBox(
       }
     }
 
-    const counterstring = counter.map((count, i) =>
-      count <= charLimit
-        ? `Line ${i + 1}: ${count} pixel`
-        : `<div class="redtext">Line ${i + 1}: ${count} pixel</div>`
-    );
+    const counterstring = charCounters.map((count, i) => `
+      <div class="${count > charLimit ? 'redtext' : ''}">
+        Line ${i + 1}: ${count} pixel --- ${padCounters[i]} pixel --- ${charLimit - count} pixel --- ${[0, 1].includes(padCounters[i] - (charLimit - count)) ? 'Y' : 'N'}
+      </div>
+    `);
 
-    const previewBoxElement = $(`#${previewBoxId}`, previewContainer);
-    previewBoxElement.find(".chars").html(picturestring);
-    previewBoxElement.find(".counter1").html(counterstring[0]);
-    previewBoxElement.find(".counter2").html(counterstring[1]);
-    previewBoxElement.find(".counter3").html(counterstring[2]);
-    previewBoxElement.find(".counter4").html(counterstring[3]);
-    previewBoxElement
-      .find(".alert")
-      .html(alert !== "" ? `Unsupported character(s): ${alert}` : "");
+    const previewBoxElement = previewContainer.querySelector(`#${previewBoxId}`);
+    previewBoxElement.querySelector(".chars").innerHTML = picturestring;
+    previewBoxElement.querySelector(".counter1").innerHTML = counterstring[0];
+    previewBoxElement.querySelector(".counter2").innerHTML = counterstring[1];
+    previewBoxElement.querySelector(".counter3").innerHTML = counterstring[2];
+    previewBoxElement.querySelector(".counter4").innerHTML = counterstring[3];
+    previewBoxElement.querySelector(".alert").innerHTML = alert !== "" ? `Unsupported character(s): ${alert}` : '';
   });
 }
 
-function renderPreview(previewContainerSelector, text, id, type) {
-  document.getElementById(previewContainerSelector).innerHTML = "";
+function renderPreview(containerSelector, text, id, type) {
+  document.getElementById(containerSelector).innerHTML = "";
   const boxType = 1;
-  staroceanPreviewBox(previewContainerSelector, text, id, boxType);
+  staroceanRenderPreview(containerSelector, text, id, boxType);
 }
 
 const charlist = [];
