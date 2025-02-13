08494303695bbde4de1e74492ed46004ab14792c
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-02-13 19:09:29 +0100

    gaia - added gaia app

---
+++ apps/gaia/config.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'gaia');
+define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
+
+define('TITLE', 'Illusion of Gaia (SNES)');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/gaia/db/gaia.sqlite3');
+define('LAST_ENTRY', 1310);
+define('NEWLINECHAR', '-----');
+define('NEWLINE_REPLACE', 0);
+
+function textClean($text) {
+  if (defined('NEWLINE_REPLACE') && NEWLINE_REPLACE && defined('NEWLINECHAR')) {
+    $text = str_replace(PHP_EOL, NEWLINECHAR, $text);
+  }
+  return $text;
+}
+
+function tableClean($text) {
+  return $text;
+}
