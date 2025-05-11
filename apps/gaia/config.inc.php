763c0268535f2e1dd35798427f73fc545d2196dd
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-05-11 17:33:58 +0200

    gaia - increased total count

--- apps/gaia/config.inc.php
+++ apps/gaia/config.inc.php
@@ -8,7 +8,7 @@ define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 define('TITLE', 'Illusion of Gaia (SNES)');
 
 define('SQLITE_FILENAME', RESOURCE_PATH . '/gaia/db/gaia.sqlite3');
-define('LAST_ENTRY', 1310);
+define('LAST_ENTRY', 1332);
 define('NEWLINECHAR', '-----');
 define('NEWLINE_REPLACE', 0);
 
