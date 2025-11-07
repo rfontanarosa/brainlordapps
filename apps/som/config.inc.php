8efd3fd590b7c2a9c71501607ac512c8d1f8a1a3
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-11-07 15:29:11 +0100

    added secret of mana app

---
+++ apps/som/config.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'som');
+define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
+
+define('TITLE', 'Secret of Mana (SNES)');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/som/db/som.sqlite3');
+define('LAST_ENTRY', 2561);
+define('NEWLINECHAR', '-----');
+define('NEWLINE_REPLACE', 0);
+
+function textClean($text) {
+  if (defined('NEWLINE_REPLACE') && NEWLINE_REPLACE && defined('NEWLINECHAR')) {
+    $text = str_replace(PHP_EOL, NEWLINECHAR, $text);
+  }
+  return $text;
+}
+
+function tableClean($text) {
+  return $text;
+}
---
+++ apps/som/tool-export-dump.php
@@ -0,0 +1,1 @@
+<?php require_once '../../common/tool-export-dump.php'; ?>
