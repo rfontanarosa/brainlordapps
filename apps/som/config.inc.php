5bb67f313eb9765fcab62acc2c8af0c25d373c96
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-01-08 11:12:59 +0100

    som - removed tools, raised limit

--- apps/som/config.inc.php
+++ apps/som/config.inc.php
@@ -9,7 +9,7 @@ define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 define('TITLE', 'Secret of Mana (SNES)');
 
 define('SQLITE_FILENAME', RESOURCE_PATH . '/som/db/som.sqlite3');
-define('LAST_ENTRY', 2614);
+define('LAST_ENTRY', 2622);
 define('NEWLINECHAR', '-----');
 define('NEWLINE_REPLACE', 0);
 
