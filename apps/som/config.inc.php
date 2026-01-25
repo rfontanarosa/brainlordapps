20b07b6a3bf33fc0eacdf4e18278a965e184eb6d
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-01-25 21:54:03 +0100

    som - increased limit

--- apps/som/config.inc.php
+++ apps/som/config.inc.php
@@ -9,7 +9,7 @@ define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 define('TITLE', 'Secret of Mana (SNES)');
 
 define('SQLITE_FILENAME', RESOURCE_PATH . '/som/db/som.sqlite3');
-define('LAST_ENTRY', 2710);
+define('LAST_ENTRY', 2728);
 define('NEWLINECHAR', '-----');
 define('NEWLINE_REPLACE', 0);
 
