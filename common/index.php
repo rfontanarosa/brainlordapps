6f859340aa9936088e21d2e5f5fc9cf42634c716
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-02-02 21:55:37 +0100

    fixed toast position

--- common/index.php
+++ common/index.php
@@ -493,15 +493,13 @@
     <?php endif; ?>
 
     <!-- TOAST -->
-    <div aria-live="polite" aria-atomic="true" class="position-relative">
-      <div class="toast-container bottom-0 end-0 p-3">
-        <div class="toast" id="my-toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-theme="dark">
-          <div class="toast-header">
-            <strong class="me-auto">Brainlordapps</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-          </div>
-          <div class="toast-body"></div>
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+      <div id="my-toast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+          <strong class="me-auto">Brainlordapps</strong>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
+        <div class="toast-body"></div>
       </div>
     </div>
 
