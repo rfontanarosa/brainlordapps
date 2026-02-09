9104b0f7aed5db397ea020587b432a50ffee6e92
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-02-09 01:34:31 +0100

    added italian previewer setting

--- apps/brainlord/config.inc.php
+++ apps/brainlord/config.inc.php
@@ -3,6 +3,7 @@
 require_once '../../config.inc.php';
 
 define('APPLICATION_ID', 'brainlord');
+define('PREVIEWER_ID', 'brainlord_it');
 define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 
 define('TITLE', 'Brain Lord (SNES)');
