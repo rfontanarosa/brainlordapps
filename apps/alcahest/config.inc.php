a3411ca01c7e8b09a49a6a952db04f84f2dfa680
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-03-29 15:10:42 +0200

    added alcahest app

---
+++ apps/alcahest/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'alcahest');
+define('PREVIEWER_ID', 'alcahest_it');
+define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
+
+define('TITLE', 'Alcahest (SNES)');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/alcahest/db/alcahest.sqlite3');
