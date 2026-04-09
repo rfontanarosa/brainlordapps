4bc49ad48d66bc967f7f81cd6cb6db39293917b7
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-04-09 10:05:35 +0200

    small fixes

--- apps/7thsaga/config.inc.php
+++ apps/7thsaga/config.inc.php
@@ -3,6 +3,7 @@
 require_once '../../config.inc.php';
 
 define('APPLICATION_ID', '7thsaga');
+define('PREVIEWER_ID', '7thsaga_it');
 define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 
 define('TITLE', '7th Saga, The (SNES)');
