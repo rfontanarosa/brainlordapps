ad1ac0fd03b6a500fd1cdf6461165f355513d0e9
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-05-06 13:12:19 +0200

    added rsaga app

---
+++ apps/rsaga/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../../config.inc.php';
+
+define('APPLICATION_ID', 'rsaga');
+define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
+
+define('TITLE', 'Romancing Saga (SNES)');
+
+define('SQLITE_FILENAME', RESOURCE_PATH . '/rsaga/db/rsaga.sqlite3');
