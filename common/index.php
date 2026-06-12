1b565e22d90c8b5befe04c9f8ebd8c4f8b34d43f
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-06-12 19:34:53 +0200

    bootstrap cache

--- common/index.php
+++ common/index.php
@@ -22,7 +22,7 @@
     <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/assets/css/bootstrap.custom.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.custom.css?<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/assets/css/bootstrap.custom.css'); ?>">
     <link rel="stylesheet" href="/node_modules/mumble-previewer/dist/css/styles.css?<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/node_modules/mumble-previewer/dist/css/styles.css'); ?>">
     <script src="/node_modules/mumble-previewer/dist/index.js?<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/node_modules/mumble-previewer/dist/index.js'); ?>"></script>
     <script src="/common/js/app.js?<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/common/js/app.js'); ?>"></script>
