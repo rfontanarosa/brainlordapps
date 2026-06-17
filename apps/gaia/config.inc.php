d49ebf862ce9971b5d7b7b7063e95e60a0cf92b3
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-06-17 19:04:26 +0200

    fixed gaia previewer

--- apps/gaia/config.inc.php
+++ apps/gaia/config.inc.php
@@ -3,6 +3,8 @@
 require_once '../../config.inc.php';
 
 define('APPLICATION_ID', 'gaia');
+define('PREVIEWER_ID', 'gaia');
+define('PREVIEWER_LANGUAGE', 'it');
 define('APPLICATION_PATH', '/apps/' . APPLICATION_ID);
 
 define('TITLE', 'Illusion of Gaia (SNES)');
